460dfd33a34fcf10b421661e41b6af2c57074b94
Catherine Salazar <catcreates13@gmail.com>
2018-01-30 03:01:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,11 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday_supplies|
     "#{season.to_s.capitalize!}"
     "#{holiday_supplies.to_s.capitalize!}"
-    
+    holiday_hash
   end
-
-
-
 
 end
 
