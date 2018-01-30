8271d8b97bb9a8dc0155b3591bcdf22ec4607fce
Catherine Salazar <catcreates13@gmail.com>
2018-01-30 03:10:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,6 +72,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  holiday_hash.find("BBQ") do |season|
+  end
 
 end
