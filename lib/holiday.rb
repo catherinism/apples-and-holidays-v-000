a6f7ea8dd43020df0c4eddda6094663c6c94ad48
Catherine Salazar <catcreates13@gmail.com>
2018-01-30 02:52:53 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,9 +60,9 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash[:winter].to_s
+holiday_hash[:winter].to_s.capitalize!
 
-  
+
 
 
 end
